846064a384d34fc57b43a15343c1369730c0e1e6
ducwwanh04 <148759677+ducwwanh04@users.noreply.github.com>
2023-11-27 16:31:15 +0700

    Update Bai3.cpp

--- BTH 5/Fromb2.cpp
+++ BTH 5/Fromb2.cpp
@@ -1,37 +1,114 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-struct Student {
+#define MAX_EMPLOYEES 100
+struct Employee {
     char name[50];
-    int age;
+    int id;
+    char position[50];
+    float salary;
 };
-int linearSearch(struct Student students[], int count, char searchName[]) {
-    for (int i = 0; i < count; i++) {
-        if (strcmp(students[i].name, searchName) == 0) {
-            return i; // Tra ve thong tin cua sinh vien qua danh sach 
+struct Employee employees[MAX_EMPLOYEES];
+int numEmployees = 0;
+void addEmployee() {
+    if (numEmployees == MAX_EMPLOYEES) {
+        printf("Khong the them nhan vien moi. Da dat gioi han.\n");
+        return;
+    }
+    struct Employee newEmployee;
+    printf("Nhap ten nhan vien: ");
+    scanf(" %[^\n]s", newEmployee.name);
+    printf("Nhap ma nhan vien: ");
+    scanf("%d", &newEmployee.id);
+    printf("Nhap chuc vu: ");
+    scanf(" %[^\n]s", newEmployee.position);
+    printf("Nhap luong: ");
+    scanf("%f", &newEmployee.salary);
+    employees[numEmployees] = newEmployee;
+    numEmployees++;
+    printf("Nhan vien da duoc them.\n");
+}
+void deleteEmployee() {
+    int employeeId, i, found = 0;
+    printf("Nhap ma nhan vien can xoa: ");
+    scanf("%d", &employeeId);
+    for (i = 0; i < numEmployees; i++) {
+        if (employees[i].id == employeeId) {
+            found = 1;
+            break;
         }
     }
-    return -1; // Khong tim thay sinh vien 
+    if (found) {
+        for (; i < numEmployees - 1; i++) {
+            employees[i] = employees[i + 1];
+        }
+        numEmployees--;
+        printf("Nhan vien da bi xoa.\n");
+    } else {
+        printf("Khong tim thay nhan vien voi ma nhan vien da cho.\n");
+    }
+}
+void displayEmployees() {
+    if (numEmployees == 0) {
+        printf("Khong co nhan vien nao trong danh sach.\n");
+        return;
+    }
+    printf("Danh sach nhan vien:\n");
+    printf("-------------------------------\n");
+    printf("Ma NV\tTen\tChuc vu\t\tLuong\n");
+    printf("-------------------------------\n");
+    for (int i = 0; i < numEmployees; i++) {
+        printf("%d\t%s\t%s\t%.2f\n", employees[i].id, employees[i].name, employees[i].position, employees[i].salary);
+    }
+    printf("-------------------------------\n");
+}
+void sortEmployeesByName() {
+    if (numEmployees == 0) {
+        printf("Khong co nhan vien nao trong danh sach.\n");
+        return;
+    }
+    for (int i = 0; i < numEmployees - 1; i++) {
+        for (int j = 0; j < numEmployees - i - 1; j++) {
+            if (strcmp(employees[j].name, employees[j + 1].name) > 0) {
+                struct Employee temp = employees[j];
+                employees[j] = employees[j + 1];
+                employees[j + 1] = temp;
+            }
+        }
+    }
+    printf("Danh sach nhan vien sau khi sap xep theo ten:\n");
+    displayEmployees();
 }
 int main() {
-    struct Student students[100];
-    int count = 0;
-    // Nhap Thong tin sinh vien 
-    strcpy(students[0].name, "Duc Anh");
-    students[0].age = 20;
-    strcpy(students[1].name, "Bach Moi");
-    students[1].age = 22;
-    strcpy(students[2].name, "Tuan tien");
-    students[2].age = 21;
-    count = 3;
-    // Tim kiem sinh vien 
-    char searchName[50];
-    printf("Nhap ten cua sinh vien can tim kiem: ");
-    scanf("%s", searchName);
-    int index = linearSearch(students, count, searchName);
-    if (index != -1) {
-        printf("Tim thay sinh vien %s Tai vi tri %d\n", searchName);
-    } else {
-        printf("Khong tim thay sinh vien %s trong danh sach\n", searchName);
+    int choice;
+    while (1) {
+        printf("\nQuan ly nhan su\n");
+        printf("1. Them nhan vien\n");
+        printf("2. Xoa nhan vien\n");
+        printf("3. Hien thi danh sach nhan vien\n");
+        printf("4. Sap xep nhan vien theo ten\n");
+        printf("5. Thoat\n");
+        printf("Nhap lua chon cua ban: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                addEmployee();
+                break;
+            case 2:
+                deleteEmployee();
+                break;
+            case 3:
+                displayEmployees();
+                break;
+            case 4:
+                sortEmployeesByName();
+                break;
+            case 5:
+                printf("Cam on da su dung chuong trinh!\n");
+                exit(0);
+            default:
+                printf("Lua chon khong hop le. Vui long chon lai.\n");
+        }
     }
     return 0;
 }
